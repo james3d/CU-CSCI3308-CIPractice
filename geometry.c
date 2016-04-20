6ee24dd4578a1eda3735764c2b784df9eea99218
james3d <james3draper@gmail.com>
2016-04-20 13:57:46 -0600

    added other files including .travis.yml

--- geometry.c
+++ geometry.c
@@ -69,3 +69,10 @@ void coord_2d_midpoint(coord_2d_t* mid, const coord_2d_t* a, const coord_2d_t* b
     mid->y = ((a->y + b->y) / 2.0 );
 
 }
+
+double coord_2d_area_triangle(const coord_2d_t *a, const coord_2d_t *b, const coord_2d_t *c) {
+	double prod1 = (a->x)*(b->y - c->y);
+	double prod2 = (b->x)*(c->y - a->y);
+	double prod3 = (c->x)*(a->y - b->y);
+	return fabs((prod1 + prod2 + prod3) / 2.0);
+}
